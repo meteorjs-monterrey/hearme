46b6e2717c97a85d3046bc728852965ad81b73bf
Fernando Flores <ferflores@gmail.com>
2015-10-10 16:16:14 -0500

    chat messages fix

--- client/js/chat/chatMessages.js
+++ client/js/chat/chatMessages.js
@@ -23,10 +23,18 @@ Template.chatMessages.events({
 Template.chatMessages.utils = {
 	sendMessage: function(){
 		var text = $(".chat-input").val();
-		var nick = Meteor.user().username || Meteor.user().profile.name;
+		var nick = "";
+
+		if(!Meteor.user().username){
+			nick = Meteor.user().username;
+		}
+
+		if((!nick || nick.length < 1) && Meteor.user().profile){
+			nick =  Meteor.user().profile.name;
+		}
 
 		if(text && text.length > 0){
-			Messages.insert({user:nick, text: text, sentOn: new Date()});
+			Messages.insert({user:nick, text: text, sentOn: new Date(), location:Geolocation.latLng()});
 			$(".chat-input").val('');
 		}
 	}
