d42c060a4421c6289e5073ec072528e88b646c78
Fernando Flores <ferflores@gmail.com>
2015-10-10 15:59:50 -0500

    mongol added

--- client/js/chat/chatMessages.js
+++ client/js/chat/chatMessages.js
@@ -10,11 +10,24 @@ Template.chatMessages.helpers({
 
 Template.chatMessages.events({
 	'click .send-message': function(){
+		Template.chatMessages.utils.sendMessage();
+	},
+
+	'keypress #chatInput': function(event){
+		if (event.which === 13) {
+			Template.chatMessages.utils.sendMessage();
+	    }
+	}
+});
+
+Template.chatMessages.utils = {
+	sendMessage: function(){
 		var text = $(".chat-input").val();
 		var nick = Meteor.user().username || Meteor.user().profile.name;
 
 		if(text && text.length > 0){
 			Messages.insert({user:nick, text: text, sentOn: new Date()});
+			$(".chat-input").val('');
 		}
 	}
-});
+};
