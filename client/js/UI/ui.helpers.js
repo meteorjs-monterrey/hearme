65f2fa60ef3cdc9ef17ce88d1017aba06399cad6
Fernando Flores <ferflores@gmail.com>
2015-10-11 11:41:25 -0500

    post dialog

--- client/js/UI/ui.helpers.js
+++ client/js/UI/ui.helpers.js
@@ -1,3 +1,7 @@
 UI.registerHelper("calculateDistance", function(lat1, lng1, lat2, lng2){
-	return locationUtils.getDistance(lat1, lng1, lat2, lng2);
+	if(lat1 && lng1 & lat2 && lng2){
+		return locationUtils.getDistance(lat1, lng1, lat2, lng2);
+	}else{
+		return "";
+	}
 });
--- client/js/home.js
+++ client/js/home.js
@@ -1,6 +1,7 @@
 Template.home.onCreated(function(){
+
 	Meteor.subscribe("userDetails");
-	Template.home.utils.updateUserGeoLocation()
+	Template.home.utils.updateUserGeoLocation();
 
 });
 
@@ -9,5 +10,28 @@ Template.home.utils = {
 		Meteor.call("updateUserGeoLocation", geoLocationUtils.latLng());
 
 		setTimeout(Template.home.utils.updateUserGeoLocation, 60000);
+	},
+
+	showPostDialog: function(){
+		var postDialogData = {
+			template: Template.post,
+			title: "Post",
+			modalDialogClass: "",
+			modalBodyClass: "",
+			modalFooterClass: "",
+			removeOnHide: true,
+			buttons:{
+				"ok":{
+					'class': 'btn-success',
+					'label': 'Hear Me!'
+				},
+				"cancel":{
+					'class': 'btn-danger',
+					'label': 'Cancel'
+				}
+			}
+		}
+
+		ReactiveModal.initDialog(postDialogData).show();
 	}
 }
