df933dc9d9b75fadb06a79399165bafe728c2f63
Oscar Islas <oscar.islaso@gmail.com>
2015-10-11 15:07:34 -0500

    fix map not loading if not cookie

--- client/js/init.js
+++ client/js/init.js
@@ -1,5 +1,9 @@
  Meteor.startup(function () {
     geoLocationUtils.update();
-    geoLocationUtils.setLocation(JSON.parse(Cookie.get('location')));
+    console.log("cookie: " + Cookie.get('location'));
+    var cookie = Cookie.get('location')
+    if(cookie != undefined){
+    	geoLocationUtils.setLocation(JSON.parse(cookie));
+	}
     GoogleMaps.load();
  });
